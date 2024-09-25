a851251c0a94cc7a332513085f03417ac93a4c3b
sonia <stejerec@myuax.com>
2024-09-25 09:48:11 +0200

    solución del problema con gradle (anteriormente no realizaba la acción de build)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+// app/build.gradle.kts
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -50,7 +52,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
